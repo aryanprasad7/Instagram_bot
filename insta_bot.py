e58735c3b10755815e45e9e4914c8c182fecc548
aryanprasad7 <61372645+aryanprasad7@users.noreply.github.com>
2020-08-23 16:24:43 +0530

    Add files via upload

---
+++ insta_bot.py
@@ -0,0 +1,55 @@
+from selenium import webdriver
+from time import sleep
+from s import pw    # this is a python file which contains contains the password of the instagram account
+
+class Instabot:
+    def __init__(self, username, pw):
+        self.driver = webdriver.Chrome()
+        self.username = username
+        self.driver.get("https://instagram.com")
+        sleep(2)
+        self.driver.find_element_by_xpath("//a[contains(text(), 'Log in')]").click()
+        sleep(2)
+        self.driver.find_element_by_xpath("//input[@name = \"username\"]").send_keys(username)
+        self.driver.find_element_by_xpath("//input[@name = \"password\"]").send_keys(pw)
+        self.driver.find_element_by_xpath('//button[@type = "submit"]').click()
+        sleep(2)
+        self.driver.find_element_by_xpath("//button[contains(text(), 'Not Now')]").click()
+        sleep(2)
+
+    def get_unfollowers(self):
+        self.driver.find_element_by_xpath("//a[contains(@href, '/{}')]".format(self.username)).click()
+        sleep(2)
+        self.driver.find_element_by_xpath("//a[contains(@href, '/following')]").click()
+        following = self._get_names()
+        self.driver.find_element_by_xpath("//a[contains(@href, '/followers')]").click()
+        followers = self._get_names()
+        not_following_back = [user for user in following if user not in followers]
+        print(not_following_back)
+
+
+    def _get_names(self):
+        # sleep(2)
+        # sugs = self.driver.find_element_by_xpath('//h4[contains(text(), Suggestions)]')
+        # self.driver.execute_script('arguments[0].scrollIntoView()', sugs)
+        sleep(1)
+        scroll_box = self.driver.find_element_by_xpath("/html/body/div[4]/div/div[2]")
+        last_ht, ht = 0, 1
+        while last_ht != ht:
+            last_ht = ht
+            sleep(1)
+            ht = self.driver.execute_script(
+                """arguments[0].scrollTo(0, arguments[0].scrollHeight);return arguments[0].scrollHeight;""", scroll_box)
+        links = scroll_box.find_elements_by_tag_name('a')
+        names = [name.text for name in links if name.text != '']
+        # print(names)
+        # close button
+        self.driver.find_element_by_xpath("/html/body/div[4]/div/div[1]/div/div[2]/button").click()
+        # at the last of the xpath sometimes '/svg' or something is added(here after button), if the code doesn't work
+        # then try removing the last /svg part and try again
+        return names
+
+
+USERNAME = 'aryanprasad_7'
+mybot = Instabot(USERNAME, pw)
+mybot.get_unfollowers()
